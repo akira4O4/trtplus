2cb5036ef3da5b1222165a3742868cee1587e928
Akira.404 <akira.404@foxmail.com>
2024-08-12 11:56:02 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -12,70 +12,6 @@
 #include "unistd.h"
 #include "vector"
 
-namespace nv = nvinfer1;
-
-void mem_test()
-{
-    std::string images_dir = "/home/seeking/llf/code/tensorRT-lab/temp/images/imagenet";
-
-    std::vector<cv::String> image_paths = get_image_paths(images_dir, "jpg");
-    std::vector<cv::Mat>    images      = get_images(image_paths);
-
-    std::vector<cv::Mat>    batch_images;
-    std::vector<cv::String> batch_images_path;
-
-    auto batch = kDefaultBatch;
-    auto c     = kDefaultChannel;
-    auto h     = kDefaultHeight;
-    auto w     = kDefaultWidth;
-
-    size_t img_size = c * h * w;
-    size_t mem_size = batch * img_size * FLOAT32;
-
-    trt::Memory mem_i(mem_size, true);
-    trt::Memory mem_o(mem_size, true);
-
-    for (int i = 0; i < images.size(); ++i)
-    {
-        INFO("Read Image: %s", image_paths.at(i).c_str());
-        batch_images_path.emplace_back(image_paths.at(i));
-        batch_images.emplace_back(images.at(i));
-
-        if (batch_images.size() != batch)
-            continue;
-
-        for (int j = 0; j < batch; ++j)
-        {
-            cv::Mat img = batch_images.at(j);
-            img         = cpu::resize(img, cv::Size(w, h));
-            img.convertTo(img, CV_32FC3, 1.f / 255.f);
-
-            size_t offset = j * img_size;
-            float* ptr    = mem_i.get_host_ptr<float>() + offset;
-            std::memcpy(ptr, img.ptr<float>(0), img_size * FLOAT32);
-        }
-
-        mem_i.to_gpu();
-        mem_i.gpu2gpu(mem_o.get_device_ptr());
-        mem_o.to_cpu();
-
-        for (int j = 0; j < batch; ++j)
-        {
-            size_t  offset = j * c * h * w;
-            auto*   ptr    = mem_o.get_host_ptr<float>() + offset;
-            cv::Mat out    = cv::Mat(h, w, CV_32FC3);
-            std::memcpy(out.ptr<float>(0), ptr, img_size);
-            out.convertTo(out, CV_8UC3, 255.0);
-
-            std::string save_dir  = "/home/seeking/llf/code/tensorRT-lab/temp/output";
-            auto        save_path = save_dir + "/bs" + std::to_string(i) + "_i" + std::to_string(j) + ".jpg";
-            cv::imwrite(save_path, out);
-        }
-        batch_images.clear();
-        batch_images_path.clear();
-    }
-}
-
 int main(int argc, char const* argv[])
 {
     auto task        = "";
@@ -105,7 +41,7 @@ int main(int argc, char const* argv[])
         model.decode_input();
         result::NCHW input = model.get_inputs()[ 0 ];
         input.info();
-        nv::Dims4 dims(batch, input.c, input.h, input.w);
+        nvinfer1::Dims4 dims(batch, input.c, input.h, input.w);
         model.set_binding_dims(input.name, dims);
     }
 
