09a1e4936430b4c90cfa2971c0539a061ad70a9f
Akira.404 <akira.404@foxmail.com>
2024-08-30 14:38:41 +0800

    Update

--- examples/yolov8_cls.cpp
+++ examples/yolov8_cls.cpp
@@ -24,39 +24,38 @@ void yolov8_cls()
     int         height      = 224;
     int         width       = 224;
 
-    //-------------------------------------------------------------------------
-
+    // Prepare Model ---------------------------------------------------------------------------------------------------
     auto model = trt::Model(model_path, device);
     model.init();
     model.show_model_info();
 
     nvinfer1::Dims4 dims(batch, channel, height, width);
     model.set_input_shape(0, dims);
-    return;
 
     auto stream = model.get_stream();
 
+    // Prepare input and output memory ---------------------------------------------------------------------------------
     result::NCHW input_shape  = model.get_inputs()[ 0 ];
     result::NCHW output_shape = model.get_outputs()[ 0 ];
 
     size_t input_size  = input_shape.NxCxHxW(kFLOAT32);
-    size_t output_size = 0;
-
-    output_size = output_shape.NxC(kFLOAT32);
+    size_t output_size = output_shape.NxC(kFLOAT32);
 
     auto model_input_memory  = std::make_shared<trt::Memory>(input_shape.idx, input_size, true, stream);
     auto model_output_memory = std::make_shared<trt::Memory>(output_shape.idx, output_size, true, stream);
 
-    //-----------------------------------------------------------------------
+    // Prepare images
+    // ---------------------------------------------------------------------------------------------------
+
     auto input_wh = cv::Size(input_shape.w, input_shape.h);
 
     std::vector<cv::String> image_paths = get_image_paths(images_dir, "jpg");
     std::vector<cv::Mat>    images      = get_images(image_paths);
 
-    std::vector<cv::Mat>    batch_images;
-    std::vector<cv::String> batch_images_path;
-
+    std::vector<cv::Mat>     batch_images;
+    std::vector<cv::String>  batch_images_path;
     std::vector<std::string> labels = load_label_from_txt(labels_file);
+
     for (int i = 0; i < images.size(); ++i)
     {
 
@@ -77,14 +76,14 @@ void yolov8_cls()
 
             out = cpu::resize(img, input_wh);
             out = cpu::image2rgb(out);
-            out = cpu::normalize(out);
+            out = cpu::normalize(out); // 1/255
 
+            // change channel
             size_t offset = n * input_shape.CxHxW();
-            //            host_ptr += n * input_shape.CxHxW();
             cpu::hwc2chw_v1(out, host_ptr + offset);
         }
 
-        // Infer--------------------------------------------------------------------------------------------------------
+        // Forward -----------------------------------------------------------------------------------------------------
         model_input_memory->to_gpu();
         void* bindings[ 2 ]{model_input_memory->get_device_ptr(), model_output_memory->get_device_ptr()};
         model.forward(bindings, stream, nullptr);
